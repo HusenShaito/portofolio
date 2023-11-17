b609bd4f412e6af0bd268c2297cc5e19b1c583ad
Husen Shaito <husenshaito@gmail.com>
2023-11-17 21:13:58 +0200

    changing the login place of session start

--- login.php
+++ login.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php 
 session_start();
 if($_SERVER['REQUEST_METHOD']== 'POST'){
@@ -17,6 +15,9 @@ if($_SERVER['REQUEST_METHOD']== 'POST'){
 
 
 }?>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
